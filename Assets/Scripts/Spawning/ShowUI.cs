67ee2e2dd7b66ced1f662592a9d41bf6be0da32b
LVFreire <lisaverafreire@gmail.com>
2025-05-24 16:07:09 -0300

    ShowUIScriptAdded

---
+++ Assets/Scripts/Spawning/ShowUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+
+    public GameObject UI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UI.SetActive(false);
+        }
+    }
+}
